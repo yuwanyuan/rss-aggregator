b41361b10bfb83f99ef643d9f9bb4f5dfada1122
yuwanyuan <yuwanyuan@users.noreply.github.com>
2026-04-14 23:29:35 +0800

    feat: add feed persistence (localStorage + DEFAULT_FEEDS env)

--- src/cf-worker.js
+++ src/cf-worker.js
@@ -2,7 +2,7 @@ import { handleRequest } from './shared.js';
 
 export default {
   async fetch(request, env, ctx) {
-    const { status, headers, body } = await handleRequest(request.url);
+    const { status, headers, body } = await handleRequest(request.url, env);
     return new Response(body, { status, headers });
   },
 };
