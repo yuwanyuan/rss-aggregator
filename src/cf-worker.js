bf5c713820f1fd221c70024b9577e93fc1efbceb
yuwanyuan <yuwanyuan@users.noreply.github.com>
2026-04-14 22:30:05 +0800

    fix: bug fixes + add deploy buttons

--- src/cf-worker.js
+++ src/cf-worker.js
@@ -1,7 +1,9 @@
+import { handleRequest } from './shared.js';
+
 export default {
   async fetch(request, env, ctx) {
-    const { handleRequest } = await import('./shared.js');
     const { status, headers, body } = await handleRequest(request.url);
     return new Response(body, { status, headers });
   },
 };
+
